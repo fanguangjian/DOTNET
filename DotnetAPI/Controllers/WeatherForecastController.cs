533a0403237ad06471d130a45de541d611149f66
G.F <fanguangjianauto@163.com>
2024-09-12 21:37:14 +1000

    Run the API

--- DotnetAPI/Controllers/WeatherForecastController.cs
+++ DotnetAPI/Controllers/WeatherForecastController.cs
@@ -7,4 +7,31 @@ namespace DotnetAPI.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase {
 
+    private readonly string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    [HttpGet("", Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> GetFiveDayForecast(){
+
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+        return forecast;
+    }
+
+ 
 }
+
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
--- DotnetAPI/readme.md
+++ DotnetAPI/readme.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: G.F
  * @Date: 2024-09-11 21:32:22
- * @LastEditTime: 2024-09-11 21:55:41
+ * @LastEditTime: 2024-09-12 21:34:53
  * @LastEditors: your name
  * @Description: 
  * @FilePath: /dotnet/DotnetAPI/readme.md
@@ -9,5 +9,6 @@
 dotnet new webapi -n DotnetAPI
 
 run: dotnet run
+dotnet watch run
 http://localhost:5121/weatherforecast
 http://localhost:5121/swagger/index.html
