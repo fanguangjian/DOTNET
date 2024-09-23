1be2623fb1f0c77d46d84c06395e88981b3fe02a
G.F <fanguangjianauto@163.com>
2024-09-23 21:33:40 +1000

    Add Cors Policy

--- DotnetAPI/Program.cs
+++ DotnetAPI/Program.cs
@@ -7,16 +7,35 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors((options)=> {
+    options.AddPolicy("DevCors", (corsBuilder) => {
+        corsBuilder.WithOrigins("Http://localhost:4200", "Http://localhost:3000" ,"Http://localhost:8000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+    });
+
+    options.AddPolicy("ProdCors", (corsBuilder) => {
+        corsBuilder.WithOrigins("Http://mysite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{   
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseCors("ProdCors");
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 
 app.MapControllers();
 
