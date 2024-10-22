ae3a538f542fc84d28e073666bd485c087724d7f
G.F <fanguangjianauto@163.com>
2024-10-22 21:48:33 +1100

    DTO

--- DotnetAPI/Controllers/UserController.cs
+++ DotnetAPI/Controllers/UserController.cs
@@ -89,7 +89,7 @@ public class UserController : ControllerBase {
     }
 
     [HttpPost("AddUser")]
-    public IActionResult AddUser(User user){
+    public IActionResult AddUser(UserToAddDto user){
         string sql = @"
         INSERT INTO [TutorialAppSchema].[Users](        
             [FirstName],
---
+++ DotnetAPI/Dtos/UserToAddDto.cs
@@ -0,0 +1,27 @@
+namespace DotnetAPI
+{
+    public partial class UserToAddDto{       
+        public string FirstName{ get; set; }
+        public string LastName{ get; set; }
+        public string Email{ get; set; }
+        public string Gender{ get; set; }
+        public bool Active{ get; set; }
+        
+        // Actually, the DB table schema can set up not NULL, default ""  
+        public UserToAddDto(){
+            if(FirstName == null ){
+                FirstName = "";
+            }
+            if(LastName == null ){
+                LastName = "";
+            }
+            if(Email == null ){
+                Email = "";
+            }
+            if(Gender == null ){
+                Gender = "";
+            }
+            
+        }
+    }
+}
